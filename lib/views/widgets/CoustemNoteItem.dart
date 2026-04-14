fbb7f7052557cd87add3dfc7563ddb97ebc6d4bc
Essam EL-Din <essameldinelshreif@gmail.com>
2026-04-14 17:14:24 +0200

    item note caed

--- lib/views/widgets/CoustemNoteItem.dart
+++ lib/views/widgets/CoustemNoteItem.dart
@@ -1,18 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:notes/views/widgets/CoustemAppBar.dart';
-
-class NotesViewBody extends StatelessWidget {
-  const NotesViewBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(children: const [CoustemAppBar(), NoteItem()]),
-    );
-  }
-}
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
